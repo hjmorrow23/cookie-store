a6f5eb52e65c595e2e288244ad77dda1b76156a5
hjmorrow23 <hjmorrow23@gmail.com>
2018-12-09 15:53:07 -0500

    React, Babel and Webpack setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+import RedHeading from 'styles/Headers';
+import styled from "styled-components";
+
+const RedHeading = styled.h1`
+    color: red;
+`;
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App"> 
+        <RedHeading> Hello, World Man! </RedHeading>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
---
+++ src/styles/Headers.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RedHeading = styled.h1`
+    color: red;
+`;
+
+export default RedHeading;
